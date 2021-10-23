ed81240e6968bac623887621f6b499cf7b1cebd4
codehiker1997 <faizantanveer84@gmail.com>
2021-10-23 11:17:04 -0700

    This is my Third commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# This is template for the message
+
+
 msg_template = """
 Hello {name},
 This is Faizan Tanveer and having a request of fee concession from your collage
@@ -6,6 +9,9 @@ Thank You,
 {name2}
 
 """
+
+
+# This is the function to format the msg present in python
 def msg_format(name = "Google", website = "www.google.com", name2 = "Faizan Tanveer"):
     my_msg = msg_template.format(name = name, website = website, name2= name2)
     print(my_msg)
@@ -13,7 +19,7 @@ def msg_format(name = "Google", website = "www.google.com", name2 = "Faizan Tanv
 
 msg_format()
 
-
+# This is the function to check what is the funciton of *args and **kwargs
 def my_format(*args, **kwargs):
     print(args, kwargs)
 
